8f4c634e8f08f938e7b8fa5682a4c7818acd2294
ZeyuL01 <zeyul@smu.edu>
2023-07-05 16:01:14 -0500

    add rmarkdown

--- R/Demo.R
+++ R/Demo.R
@@ -24,14 +24,27 @@ Demo = function(N,dat=c("CTCF","ZBTB7A","KDM1A")){
   return(results)
 }
 
-Show_Results<-function(dat,TF_names=CTCF_Demo$TF,burnin){
+Show_Results<-function(dat,burnin){
+  TF_names=CTCF_Demo$TF
   theta_ij_mat<-dat$theta_ij
   theta_i_mat<-dat$theta_i
-  tf_results<-theta_ij_mat[,(dim(theta_ij_mat)[2]-burnin):dim(theta_ij_mat)[2]]
-  tf_results_i<-theta_i_mat[,(dim(theta_i_mat)[2]-burnin):dim(theta_i_mat)[2]]
-  results_tab<-data.frame(TF=TF_names,res=rowMeans(tf_results),res_i=rowMeans(tf_results_i))
-  results_tab$rank=rank(-results_tab$res)
-  return(results_tab)
+  tf_results_ij<-rowMeans(theta_ij_mat[,(dim(theta_ij_mat)[2]-burnin):dim(theta_ij_mat)[2]])
+  tf_results_i<-rowMeans(theta_i_mat[,(dim(theta_i_mat)[2]-burnin):dim(theta_i_mat)[2]])
+
+  results_theta_ij=data.frame(TF=TF_names,Theta_ij=tf_results_ij,Rank_ij=rank(-tf_results_ij))
+  results_theta_ij=results_theta_ij[order(-results_theta_ij$Theta_ij),]
+  row.names(results_theta_ij) <- NULL
+
+  results_theta_i=data.frame(TF=TF_names,Theta_i=tf_results_i)
+  results_theta_i=results_theta_i[!duplicated(results_theta_i$TF),]
+  results_theta_i=results_theta_i[order(-results_theta_i$Theta_i),]
+  results_theta_i$Rank_i=rank(-results_theta_i$Theta_i)
+  row.names(results_theta_i) <- NULL
+
+  return_list<-list()
+  return_list[["Theta_ij"]]<-results_theta_ij
+  return_list[["Theta_i"]]<-results_theta_i
+  return(return_list)
 }
 
 
